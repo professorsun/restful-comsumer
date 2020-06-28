f743ddb472dbfee3fa1b33db58515c2f6ef2b7a6
409298131@qq.com <409298131@qq.com>
2020-06-29 00:12:26 +0800

    add cookie and session

--- src/main/java/com/uocloud/consumer/controller/UserController.java
+++ src/main/java/com/uocloud/consumer/controller/UserController.java
@@ -18,7 +18,7 @@ import javax.servlet.http.HttpSession;
 import java.util.*;
 
 @Controller
-public class registerController {
+public class UserController {
 
     @Autowired
     private RestTemplate restTemplate;
@@ -32,12 +32,15 @@ public class registerController {
 
 
     @RequestMapping("/adduser")
-    public String register(HttpServletRequest request, Map<String,Object> map){
+    public String register(HttpServletRequest request, HttpServletResponse response,Map<String,Object> map){
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 //        String password2 = request.getParameter("password2");
 //        String userPhone = request.getParameter("userPhone");
-
+        User user=new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        HttpSession session = request.getSession();
         String s = restTemplate.postForObject("http://localhost:9000/user/adduser?username="+username+"&password="+password, null, String.class);
         System.out.println(s);
 //        ModelAndView mv = new ModelAndView("register");
@@ -45,7 +48,8 @@ public class registerController {
         if("the user is registered".equals(s)){
             return "register";
         }else {
-            return "index";
+
+            return "login";
         }
     }
 
@@ -95,5 +99,49 @@ public class registerController {
 
             return "register";
         }
+    }
+    @GetMapping("/login")
+
+    @RequestMapping("/login")
+    public String login(HttpServletRequest request,
+                              HttpServletResponse response,
+                              Map<String,Object> map){
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        User dbUser = restTemplate.postForObject(
+                "http://localhost:9000/user/getuser?username=" + username + "&password=" + password,
+                null,User.class);
+        HttpSession session = request.getSession();
+        if (dbUser != null){  //user不为空，说明用户合法，进行更新操作
+            String toc= UUID.randomUUID().toString();
+            dbUser.setTocken(toc);
+            System.out.println(toc);
+//            restTemplate.postForObject("http://localhost:9000/user/updateuser?username="+username+"&tocken="+toc,null, Integer.class);
+            Cookie cookie=new Cookie("tocken",toc);
+            cookie.setMaxAge(60*60*24*7);       //设置cookie保存时间为一周
+            response.addCookie(cookie);
+            session.setAttribute("user", dbUser);
+            map.put("msg", "登陆成功");
+
+            return "index";
+        }else{
+            map.put("msg","密码或用户名错误！");
+
+            return "login";
+        }
+
+    }
+
+
+    @RequestMapping("/logout")
+    public String logout(HttpServletRequest request,
+                         HttpServletResponse response){
+        request.getSession().removeAttribute("user");   //移除session
+        Cookie cookie = new Cookie("token", null);  //要删除cookie需要新建一个同名的cookie，并将value设置为null
+        cookie.setMaxAge(0);    //立即删除型
+        response.addCookie(cookie);//删除cookie
+        return "redirect:/";
+
     }
 }
